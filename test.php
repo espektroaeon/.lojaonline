e5e255901ef27bacb6df5867e2d053769c380a39
espektroaeon <103334163+espektroaeon@users.noreply.github.com>
2022-04-10 19:35:56 -0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php 
+echo "TESTE";
+
+?>
+
+<html>
+  
+  <body>
+    Cabra
+    <?php include 'https://xpmh.me/'; ?>
+    <iframe src="https://google.com" style="width:100vw; height:100vh"></iframe>
+    
+  </body>
+</html>
